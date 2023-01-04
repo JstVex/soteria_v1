872c070b350155d49eee62828f1cbf5d587dd4e4
Jst Vex <heinhtetlulu14@gmail.com>
2023-01-04 20:48:22 +0630

    added padaytharpin website

---
+++ pages/youtube/padaytharpin.js
@@ -0,0 +1,30 @@
+import styles from "../../styles/Channel.module.css";
+import Videos from "../../components/Videos";
+
+const Weclick = ({ vids }) => {
+    return (
+        <div className={styles.container} >
+            <div className={styles.header}>
+                <img src={vids[0].pfp} alt="" className={styles.pfp} />
+                <div className={styles.wrap}>
+                    <span className={styles.channel}>{vids[0].channel.toUpperCase()}</span>
+                    <span className={styles.subs}>{vids[0].subs}</span>
+                </div>
+            </div>
+            <div className={styles.heading}>
+                Videos
+            </div>
+            {vids && vids.map((vid) => {
+                return <Videos vid={vid} key={vid._id} />
+            })}
+        </div >
+    );
+}
+
+export async function getServerSideProps() {
+    const res = await fetch(`https://soteria-backend-alc9.onrender.com/channels/padaytharpin`);
+    const vids = await res.json();
+    return { props: { vids } }
+}
+
+export default Weclick;
