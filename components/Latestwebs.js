1c237d5a0faeb04cf70b99e279b49d5c43793da1
JstVex <heinhtetlulu14@gmail.com>
2023-08-01 23:45:02 +0630

    Display unavailable if the website is not working

--- components/Latestwebs.js
+++ components/Latestwebs.js
@@ -2,12 +2,22 @@ import styles from "../styles/Websites.module.css"
 
 const Lastestwebs = ({ web }) => {
     console.log(web)
+
+    if (web.titles[0] === undefined) {
+        return (
+            <div className={styles.card_warn}>
+                <p className={styles.warn}>
+                    {web.website} is currently not available
+                </p>
+            </div>
+        )
+    }
     return (
         <a href={web.urls[0]} target="_blank">
             <div className={styles.card}>
                 <img src={web.imgs[0]} alt="" className={styles.img} />
                 <p className={styles.title}>{web.titles[0]}</p>
-                <span className={styles.text}>{web.texts[0].slice(0, 150) + '...'}</span>
+                <span className={styles.text}>{web.texts[0]?.slice(0, 150) + '...'}</span>
                 <div className={styles.extra}>
                     <span className={styles.date}>{web.dates[0]}</span>
                     <span className={styles.name}>{web.website}</span>
--- pages/websites/[website].js
+++ pages/websites/[website].js
@@ -2,14 +2,19 @@ import styles from "../../styles/Website.module.css";
 import Posts from "../../components/Posts";
 
 const Website = ({ posts }) => {
-    console.log(posts)
+    console.log('posts', posts)
     let slide = -1;
     return (
         <div className={styles.container} >
             <div className={styles.header}>
                 <p className={styles.website}>{posts[0].website.toUpperCase()}</p>
             </div>
-            {posts && posts.map((post) => {
+            {posts[0].titles.length === 0 && (
+                <div className={styles.warn}>
+                    Sorry, {posts[0].website} is not available right now.
+                </div>
+            )}
+            {posts && posts[0].titles.length !== 0 && posts.map((post) => {
                 slide++;
                 return <Posts post={post} key={post._id} slide={slide} />
             })}
