8eb89e40f513e5b240d8161629f7757d442c9bc5
JstVex <heinhtetlulu14@gmail.com>
2023-09-30 14:27:17 -0700

    Add and fix configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'onedrive.live.com',
+        port: '',
+        pathname: '/**/**',
+      },
+    ],
+  },
   reactStrictMode: true,
 }
 
