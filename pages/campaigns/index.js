b464d682d5b9eb630941cf527872aadce26d94d6
JstVex <heinhtetlulu14@gmail.com>
2023-09-30 21:26:29 -0700

    Make the images scroll automatically in a loop

--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -1,7 +1,7 @@
 import styles from "../../styles/CampaignMain.module.css"
 import CampaignImages from "../../components/CampaignImages";
 import Campaign from "../../components/Campaign";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import ModalCampaign from "../../components/ModalCampaign";
 
 const Campaigns = ({ campaigns }) => {
@@ -17,6 +17,13 @@ const Campaigns = ({ campaigns }) => {
     const [selectedPrize, setSelectedPrize] = useState(null);
     const [selectedForwhom, setSelectedForwhom] = useState(null);
 
+    function scrollToSection(sectionId) {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.first_section}>
@@ -25,22 +32,60 @@ const Campaigns = ({ campaigns }) => {
                         Many campaigns are available as well
                     </p>
                     <p className={styles.subtext}>
-                        To persuade more people into donations many individuals and organizations have came up with campaigns which are basically donations but with prizes back. These are in the form of lotteries tickets for big prizes or gift cards or subscriptions or many more. more stuff here to put
+                        To persuade more people into donations, many individuals and organizations have came up with campaigns which are basically donations but with prizes. These are in the form of lotteries tickets for big prizes or gift cards or subscriptions or many more.
                     </p>
 
                     <button className={styles.button}>
-                        <a href="#explore"> Explore campaigns</a>
+                        <div onClick={() => scrollToSection('explore')}>
+                            Explore campaigns
+                        </div>
                     </button>
                 </div>
-                <div className={styles.imageSection}>
-                    {campaigns && campaigns.map((campaign) => {
-                        return <CampaignImages campaign={campaign} key={campaign._id} />
+
+                <div className={styles.imageSection} style={{ width: `${campaigns.length * 200}%` }}>
+                    {campaigns.map((campaign, index) => {
+                        return (
+                            <CampaignImages campaign={campaign} key={campaign._id + index} />
+                        );
                     })}
+                    {/* Duplicate the images multiple times to ensure an infinite loop */}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate1" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate2" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate3" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate4" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate5" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate6" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate7" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate8" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate9" + index} />
+                    ))}
+                    {campaigns.map((campaign, index) => (
+                        <CampaignImages campaign={campaign} key={campaign._id + "duplicate10" + index} />
+                    ))}
                 </div>
             </div>
             <div className={styles.second_section} id='explore'>
                 <div className={styles.heading}>
-                    <span className={styles.heading_content}>Campaigns</span>
+                    <span className={styles.heading_content}>
+                        Campaigns
+                    </span>
                 </div>
                 <div className={styles.content}>
                     {campaigns && campaigns.map((campaign) => {
@@ -54,7 +99,6 @@ const Campaigns = ({ campaigns }) => {
 
     );
 }
-
 
 export async function getServerSideProps() {
     const res = await fetch(`http://localhost:4004/campaigns`)
