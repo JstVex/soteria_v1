86b4e0e7f614fb0da2ee777004f1b25af7bce055
JstVex <heinhtetlulu14@gmail.com>
2023-10-09 11:08:24 -0700

    Create Loading component

---
+++ components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className={styles.loading}>
+            <p>Loading...</p>
+        </div>
+    );
+};
+
+export default Loading;
